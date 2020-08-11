50cd5656995eb882a5cee19b5d1092ddca64733f
jadehendricks <hendricksjade@gmail.com>
2020-08-12 01:26:59 +0200

    adding in my repos to the index page

--- main.js
+++ main.js
@@ -6,3 +6,13 @@ $('.navigation-menu a').on('click', function(e) {
         }, 800);
     }
 });
+
+const getGithubInformation = async () => {
+    const res = await fetch('https://api.github.com/users/JadeHendricks/repos');
+    const data = await res.json();
+    const repos = document.getElementById('repos');
+
+    repos.innerHTML = `${data.map(repo => `<a href='${repo.html_url}'>${repo.name}</a>`)}`
+}
+
+getGithubInformation();
